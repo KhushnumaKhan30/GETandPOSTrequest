7c998114164251694e05be8522a18e59a7e9dfc4
khushnuma khan <khankhushnuma400@gmail.com>
2026-02-08 01:22:23 +0530

    commit with frontend and backend

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get('/register', (req, res) => {
+    let {user, password} = req.query;
+  res.send(`This is a GET request to /register with user: ${user} and password: ${password}`);
+});
+
+app.post('/register', (req, res) => {
+  let {user, password} = req.body;
+  res.send(`This is a POST request to /register with user: ${user} and password: ${password}`);
+});
